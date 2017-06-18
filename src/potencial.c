5d8dd008b8891e9fe8a0c1a5a26acaadb2bac65c
ignaciosticco <ignaciosticco@gmail.com>
2017-06-18 18:09:37 -0300

    incorporacion de comentarios

--- src/potencial.c
+++ src/potencial.c
@@ -3,11 +3,15 @@
 #include <stdlib.h>
 #include <math.h>
 
-float rin = 1.8;
-float rout = 2.0;
+float rin = 1.8;  //  r de corte interno
+float rout = 2.0; //  r de corte interno (V=0 si r>r0ut)
 float sigma = 1;
 
-
+/*
+ Devuelve el potencial que siente cada molecula, producto de la interaccion de 
+ Lenard-jones con todos sus vecinos. El output es un vector de n potenciales
+ (uno para cada particula)
+*/
 void potencial(float *vector_x,float *vector_y,float *vector_z, float *vector_potencial,int n){
 	float deltax,deltay,deltaz,r,s;
 	for (int i = 0; i < n; ++i){
@@ -16,7 +20,7 @@ void potencial(float *vector_x,float *vector_y,float *vector_z, float *vector_po
 			deltay = vector_y[i] - vector_y[j];
 			deltaz = vector_z[i] - vector_z[j];
 			r = sqrt(deltax*deltax + deltay*deltay + deltaz*deltaz);
-			if (r<rin && r!=0){
+			if (r<rin && r>0){
 				vector_potencial[i]+=4*(pow(sigma/r,12) - pow(sigma/r,6));
 			}
 			else if(rin<r && r<rout){  
@@ -31,7 +35,10 @@ void potencial(float *vector_x,float *vector_y,float *vector_z, float *vector_po
 	return;
 }
 
-// Esta es una funcion de spline.
+/*
+Esta es una funcion de spline. Fuente: http://www4.ncsu.edu/~franzen/public_html/CH795N/lecture/III/trunc/trunc.html
+Suaviza al potencial desde rin hasta rout. 
+*/
 float switching_function(float r,float rin,float rout){
 	 float numerador1, numerador2,denominador,resultado;
 	 numerador1 = (rout*rout-r*r)*(rout*rout-r*r);
